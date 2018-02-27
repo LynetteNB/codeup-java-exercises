840a4d8bc94c24378ad1b50c15541d6bfb174602
Lynette <lyna8907@gmail.com>
2018-02-27 11:56:13 -0600

    java primitive types exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int myFavoriteNumber = 52;
+        String myString = "Lynette is coding!";
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
+        System.out.println(x);
     }
 }
