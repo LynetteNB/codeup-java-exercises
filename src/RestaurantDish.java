06d1d730b12701d57bc080413dc3939591d0b2d9
Lynette <lyna8907@gmail.com>
2018-03-02 12:29:12 -0600

    refactored restaurant dish and test to use private, getters, and setters

--- src/RestaurantDish.java
+++ src/RestaurantDish.java
@@ -1,9 +1,45 @@
 public class RestaurantDish {
-    public int costInCents;
-    public String nameOfDish;
-    public boolean wouldRecommend;
+    private int costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
 
     public void eat() {
-        System.out.println("Nom nom nom!");
+        if(wouldRecommend){
+            System.out.println("Nom nom nom!");
+        } else {
+            System.out.println("No noms here!");
+        }
+    }
+
+    public RestaurantDish(int costInCents, String nameOfDish, boolean wouldRecommend) {
+        this.costInCents = costInCents;
+        this.nameOfDish = nameOfDish;
+        this.wouldRecommend = wouldRecommend;
+    }
+    public RestaurantDish(){}
+
+    public void setCostInCents(int costInCents) {
+        this.costInCents = costInCents;
+    }
+
+    public void setNameOfDish(String nameOfDish) {
+        this.nameOfDish = nameOfDish;
+    }
+
+    public void setWouldRecommend(boolean wouldRecommend) {
+        this.wouldRecommend = wouldRecommend;
+    }
+
+    public int getCostInCents() {
+
+        return costInCents;
+    }
+
+    public String getNameOfDish() {
+        return nameOfDish;
+    }
+
+    public boolean isWouldRecommend() {
+        return wouldRecommend;
     }
 }
--- src/RestaurantTest.java
+++ src/RestaurantTest.java
@@ -1,15 +1,26 @@
 public class RestaurantTest {
     public static void main(String[] args) {
         RestaurantDish dish = new RestaurantDish();
-        dish.costInCents = 150;
-        dish.wouldRecommend = true;
-        dish.nameOfDish = "Hey Hey Chicken";
-        System.out.print("The cost of " + dish.nameOfDish + " is " + dish.costInCents + " cents and ");
-        if(dish.wouldRecommend) {
+        dish.setCostInCents(150);
+        dish.setWouldRecommend(true);
+        dish.setNameOfDish("Hei Hei Chicken");
+        System.out.print("The cost of " + dish.getNameOfDish() + " is $");
+        System.out.printf("%.2f and ", (double) dish.getCostInCents()/100);
+        if(dish.isWouldRecommend()) {
             System.out.println("we would definitely recommend this dish!");
         } else {
             System.out.println("we would not recommend this dish.");
         }
         dish.eat();
+
+        RestaurantDish dish2 = new RestaurantDish(10000, "Baby You Can Chive My Car Burger", false);
+        System.out.print("The cost of " + dish2.getNameOfDish() + " is $");
+        System.out.printf("%.2f and ", (double) dish2.getCostInCents()/100);
+        if(dish2.isWouldRecommend()) {
+            System.out.println("we would definitely recommend this dish!");
+        } else {
+            System.out.println("we would not recommend this dish.");
+        }
+        dish2.eat();
     }
 }
