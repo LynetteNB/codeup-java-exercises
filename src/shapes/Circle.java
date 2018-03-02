1938ba2f19bf40fd4cc2e8cc02866cc9fb032ebc
Lynette <lyna8907@gmail.com>
2018-03-02 15:49:29 -0600

    completed oop exercises and bonuses

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,5 +1,22 @@
 package shapes;
 
 public class Circle {
+    private double radius;
+    private static int numOfCircles = 0;
 
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference() {
+        return Math.PI * 2 * radius;
+    }
+    public static void addACircle() {
+        numOfCircles += 1;
+    }
+    public static int getNumOfCircles(){
+        return numOfCircles;
+    }
 }
---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,17 @@
+package shapes;
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Input scanner = new Input();
+        do {
+            double radius = scanner.getDouble("What is the radius of the circle? ");
+            Circle circle = new Circle(radius);
+            Circle.addACircle();
+            System.out.printf("The area of a circle with a radius of " + radius + " is %.2f.\n", circle.getArea());
+            System.out.printf("The circumference of a circle with a radius of " + radius + " is %.2f.\n", circle.getCircumference());
+        } while (scanner.yesNo("Would you like to make another circle? "));
+        System.out.printf("You made " + Circle.getNumOfCircles() + " circles!");
+    }
+
+}
