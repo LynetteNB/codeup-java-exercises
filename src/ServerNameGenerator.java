08f8b7321a92aac178fba81ee3a28ced6c448baa
Lynette <lyna8907@gmail.com>
2018-03-05 10:44:00 -0600

    completed server name generator exercise

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,12 @@
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+        String[] adjectives = {"nice", "mean", "pretty", "ugly", "responsible", "ambitious", "pale", "tan", "productive", "lazy"};
+        String[] nouns = {"man", "woman", "girl", "boy", "restaurant", "car", "cup", "phone", "computer", "planner"};
+        System.out.println("Here is your server name:");
+        System.out.println(adjectives[getRandomNum()] + " - " + nouns[getRandomNum()]);
+
+    }
+    public static int getRandomNum(){
+        return (int) Math.floor(Math.random()*10);
+    }
+}
